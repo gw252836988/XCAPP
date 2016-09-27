33fc0c758ca9b41280af6219ddc39078da3c7e18
新城集团 <xcjt@promote.cache-dns.local>
2016-09-27 09:16:27 +0800

    Initial Commit

---
+++ XCAPP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XCAPP
+//
+//  Created by 新城集团 on 16-9-27.
+//  Copyright (c) 2016年 新城集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XCAPP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XCAPP
+//
+//  Created by 新城集团 on 16-9-27.
+//  Copyright (c) 2016年 新城集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
